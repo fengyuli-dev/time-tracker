a6baf587cf1b513922802b548d33e19761890b6e
Li Fengyu <fengyuli2002@gmail.com>
2021-07-22 17:37:34 +0800

    Change icons in nav bar.

--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -3,8 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import WorkIcon from '@material-ui/icons/Work';
+import AssessmentIcon from '@material-ui/icons/Assessment';
 import "./navigationBar.css"
 
 const useStyles = makeStyles({
@@ -27,8 +27,8 @@ export default function SimpleBottomNavigation() {
       className={classes.root}
     >
       <BottomNavigationAction label="Timer" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Activities" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Report" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Activities" icon={<WorkIcon />} />
+      <BottomNavigationAction label="Report" icon={<AssessmentIcon />} />
     </BottomNavigation>
   );
 }
